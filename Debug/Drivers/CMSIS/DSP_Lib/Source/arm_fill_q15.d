4b0188e5108f11df7aecab9325bad0b6eb1bc5d2
Eyal Meschel <eyalm@vectoriousmedtech.com>
2025-01-27 13:51:51 +0200

    add preprocessor define symbols and git ignore

--- Debug/Drivers/CMSIS/DSP_Lib/Source/arm_fill_q15.d
+++
@@ -1,4 +0,0 @@
-Drivers/CMSIS/DSP_Lib/Source/arm_fill_q15.o: \
- ../Drivers/CMSIS/DSP_Lib/Source/arm_fill_q15.c \
- ../Drivers/CMSIS/DSP_Lib/Source/arm_math.h
-../Drivers/CMSIS/DSP_Lib/Source/arm_math.h:
--- Debug/Drivers/CMSIS/DSP_Lib/Source/arm_min_q31.d
+++
@@ -1,4 +0,0 @@
-Drivers/CMSIS/DSP_Lib/Source/arm_min_q31.o: \
- ../Drivers/CMSIS/DSP_Lib/Source/arm_min_q31.c \
- ../Drivers/CMSIS/DSP_Lib/Source/arm_math.h
-../Drivers/CMSIS/DSP_Lib/Source/arm_math.h:
